353dc6d34f5b522e289dfe4b4c3aa4923039ccdb
Daniela Madalena <dani.m.madalena@gmail.com>
2022-07-02 23:26:46 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,50 @@
+
+
+alert("Bienvenido a centro de adopción de mascotas");
+let contraseña = "entrar01";
+
+for (let i = 2; i >= 0; i--) {
+  let ingreso = prompt("Ingresá tu contraseña");
+  if (contraseña == ingreso) {
+    alert("Bienvenido/a");
+
+    let edad = prompt("Por favor ingresá tu edad:");
+
+    while (edad < 18) {
+      alert("No podés adoptar un animal siendo menor de 18 años, lo siento");
+      edad = prompt("Por favor ingresá tu edad:");
+    }
+
+    let pedido = prompt(
+      "Por favor elegí que animal queres adoptar:\n1. Perro \n2. Gato \n3. Conejo"
+    );
+
+    switch (pedido) {
+      case "1":
+        alert(
+          "Has adoptado un perrito llamado Rocko, pronto llegará a su nuevo hogar"
+        );
+        break;
+      case "2":
+        alert(
+          "Has adoptado un gatito llamado Pepe, pronto llegará a su nuevo hogar"
+        );
+        break;
+      case "3":
+        alert(
+          "Has adoptado un conejito llamado Julio, pronto llegará a su nuevo hogar"
+        );
+        break;
+      default:
+        alert("Lo siento, el numero elegido no es correcto");
+        break;
+    }
+    break;
+  } else {
+    alert(
+      "Tu contraseña es incorrecta, vuelve a intentarlo, te quedan " +
+      i +
+      " intentos"
+    );
+  }
+}

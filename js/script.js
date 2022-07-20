5330da601d442072cc0b6bf677c73e2c2b39a7df
Daniela Madalena <dani.m.madalena@gmail.com>
2022-07-19 23:23:28 -0300

    agregando class

--- js/script.js
+++ js/script.js
@@ -3,15 +3,13 @@
 
 alert("Bienvenido al centro de préstamos del Banco JavaScript");
 
-let contraseña = "entrar01";
-
 let nombre = prompt("Por favor ingrese su nombre:");
 
 let cuenta = prompt(nombre + " es usted cliente de nuestro banco?\n1. Si, lo soy\n2. No, no lo soy");
 
 /* Array con objetos dentro: */
 
-const tarjetaCredito = [{ plan: 'Básico', limiteExtraccion: 20000, limiteCompra: 100000, limiteCuotas: 90000, descripcion: 'Plan básico, sencillo y cómodo, cuenta con un límite para compras en cuotas y en un solo pago reducido para el uso conservado.' }, { plan: 'Platino', limiteExtraccion: 40000, limiteCompra: 280000, limiteCuotas: 260000, descripcion: 'Plan aumentado, un plan con un límite superior para compras tanto en cuotas como en un pago.' }, { plan: 'Dorado', limiteExtraccion: 80000, limiteCompra: 400000, limiteCuotas: 390000, descripcion: 'Plan pre full, un plan con un límite extendido y holgado para la comodidad del usuario.' }, { plan: 'Black', limiteExtraccion: 150000, limiteCompra: 1200000, limiteCuotas: 1000000, descripcion: 'El plan black es un plan full premium que cuenta con un límite de más de un millón de pesos para compras en un pago o en cuotas' }]; 
+const tarjetaCredito = [{ plan: 'Básico', limiteExtraccion: 20000, limiteCompra: 100000, limiteCuotas: 90000, descripcion: 'Plan básico, sencillo y cómodo, cuenta con un límite para compras en cuotas y en un solo pago reducido para el uso conservado.' }, { plan: 'Platino', limiteExtraccion: 40000, limiteCompra: 280000, limiteCuotas: 260000, descripcion: 'Plan aumentado, un plan con un límite superior para compras tanto en cuotas como en un pago.' }, { plan: 'Dorado', limiteExtraccion: 80000, limiteCompra: 400000, limiteCuotas: 390000, descripcion: 'Plan pre full, un plan con un límite extendido y holgado para la comodidad del usuario.' }, { plan: 'Black', limiteExtraccion: 150000, limiteCompra: 1200000, limiteCuotas: 1000000, descripcion: 'El plan black es un plan full premium que cuenta con un límite de más de un millón de pesos para compras en un pago o en cuotas' }];
 
 function porcentajePrestamo(numeroA, interes) {
 
@@ -24,19 +22,36 @@ let tasaInteres, mensaje;
 
 let inicioSesion = false;
 
+/* Clase con un metodo creado: */
+class Usuario {
+    constructor(usuario, contrasenia) {
+        this.usuario = usuario;
+        this.contrasenia = contrasenia;
+    }
+
+    ingresoCorrecto() {
+        alert("Bienvenida/o " + nombre);
+    }
+}
+
+const usuario1 = new Usuario('usuario01', 'entrar01');
+
 if (cuenta == 1) {
 
     tasaInteres = 8;
 
     mensaje = "Por ser cliente de nuestro banco la tasa de interés por su prestamos es solo del 8%\n\nPor favor, elija entre los valores que podemos ofrecerle:\n1. $20000\n2. $50000\n3. $80000\n4. $100000";
 
+    alert('Por favor ingrese a continuación su nombre de usuario y contraseña:');
+
     for (let i = 2; i >= 0; i--) {
 
-        entrar = prompt("Por favor ingrese su contraseña");
+        let confirmarUsuario = prompt('Ingrese su usuario:');
+        let confirmarContrasenia = prompt('Ingrese su contraseña:');
 
-        if (contraseña == entrar) {
-
-            alert("Bienvenida/o " + nombre);
+        if (confirmarUsuario == usuario1.usuario && confirmarContrasenia == usuario1.contrasenia) {
+            
+            usuario1.ingresoCorrecto();
 
             inicioSesion = true;
 
@@ -88,7 +103,7 @@ if (cuenta == 2 && edad >= 18) {
         if (contraseniaUsuario != repeticionContraseña) {
 
             alert('Lo siento las contraseñas no coinciden');
-            
+
             clienteNuevo = true;
 
         } else {
@@ -110,7 +125,7 @@ if (cuenta == 2 && edad >= 18) {
             }
 
             let nuevoCliente = prompt('Por favor indique en cual de nuestros planes está interesado:\n1. Básico \n2. Platino \n3. Dorado \n4. Black');
-            
+
             if (nuevoCliente == 1 || nuevoCliente == 2 || nuevoCliente == 3 || nuevoCliente == 4) {
 
                 alert('Muchas gracias por elegirnos, en un momento un representante se pondrá en contacto con usted al teléfono ingresado anteriormente para continuar la operación. En el mismo llamado podrá gestionar el préstamo al 8% de interés una vez dado de alta como cliente. Saludos. ');
@@ -178,7 +193,7 @@ if (inicioSesion || (cuenta == 2 && edad >= 18 && clienteNuevo == false)) {
     if (cuotas <= 18) {
 
         const filtro = cuotasDisponibles.filter((cuota) => cuota >= cuotas);
-        
+
         alert('La cantidad de cuotas disponibles son las siguientes: ' + (JSON.stringify(filtro)));
 
     } else {
